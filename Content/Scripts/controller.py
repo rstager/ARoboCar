8858a051cd8f9a4caadc8ce44858dbcf8eca1d5f
rstager <roger@stager.us>
2017-06-18 22:21:00 -0600

    checkin

---
+++ Content/Scripts/controller.py
@@ -0,0 +1,20 @@
+import h5py
+import pickle
+import numpy as np
+import sys
+
+# This controller just follows the PID recommendations
+
+fstate=open("../../roboserver.state","rb")
+fcmd=open("../../roboserver.cmd","wb")
+print("Connection opened")
+config = pickle.load(fstate)
+pickle.dump(config,fcmd)
+fcmd.flush()
+
+while True:
+    imgs = pickle.load(fstate)
+    state=pickle.load(fstate)
+    print("pathdistance {:7f} offset {:5f} distance {:7f} angle {:5.3f} dt={:5.4f}".format(state["pathdistance"], state["offset"], state["PIDthrottle"], state["PIDsteering"],state["delta_time"]))
+    pickle.dump({"steering":state["PIDsteering"],'throttle':state["PIDthrottle"]},fcmd)
+    fcmd.flush()

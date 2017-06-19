3b9e1334293088937a875fce0c46f4b81fd07aea
rstager <roger@stager.us>
2017-06-18 22:20:01 -0600

    Added config handshake

--- Content/Scripts/CNNController.py
+++ Content/Scripts/CNNController.py
@@ -4,21 +4,23 @@ import numpy as np
 import sys
 from keras.models import load_model
 
-model=load_model("model_1_save.h5")
+model=load_model("model_1_v1.h5")
 print(model.summary())
 
 fstate=open("../../roboserver.state","rb")
 fcmd=open("../../roboserver.cmd","wb")
 print("Connection opened")
 
-width,height = pickle.load(fstate)
-print(width,height)
+config = pickle.load(fstate)
+pickle.dump(config,fcmd)
+fcmd.flush()
+
 
 while True:
     img= pickle.load(fstate)
     state=pickle.load(fstate)
     img=np.reshape(img,(1,img.shape[0],img.shape[1],img.shape[2]))
-    steering=model.predict(img)[0]
-    print(" reward {:7f} offset {:5f} distance {:7f} angle {:5.3f} dt={:5.4f}".format(state["reward"], state["offset"], state["PIDthrottle"], state["PIDsteering"],state["delta_time"]))
+    steering=model.predict(img)[0,0]
+    print("steering {:5.3f} pathdistance {:7f} offset {:5f} PID {:5.3f} {:5.3f} dt={:5.4f}".format(steering,state["pathdistance"], state["offset"], state["PIDthrottle"], state["PIDsteering"],state["delta_time"]))
     pickle.dump({"steering":steering,'throttle':state["PIDthrottle"]},fcmd)
     fcmd.flush()
